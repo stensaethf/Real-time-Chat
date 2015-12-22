d3c107d97f26c6d5deba51ee0a5dec449cde4831
Frederik Stensaeth <stensaethf@carleton.edu>
2015-12-22 22:09:42 +0100

    can now insert data to db

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log('Worked');
+var mongo = require('mongodb').MongoClient;
+var client = require('socket.io').listen(8080).sockets;
+
+mongo.connect('mongodb://127.0.0.1/chat', function(err, db) {
+	if(err) throw err;
+
+	client.on('connection', function(socket) {
+		
+		var col = db.collection('messages');
+
+		// wait for input
+		socket.on('input', function(data) {
+			var name = data.name;
+			var message = data.message;
+			var whitespacePattern = /^\s*$/;
+
+			if (whitespacePattern.test(name) || whitespacePattern.test(message)) {
+				console.log('invalid data');
+			} else {
+				col.insert({name : name, message : message}, function() {
+					console.log('inserted');
+				});
+			}
+		});
+	});
+});
+
+
