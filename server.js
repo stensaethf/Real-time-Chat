727966c9875f8eed75acff120e3ee68ec49f49cd
Frederik Stensaeth <stensaethf@carleton.edu>
2015-12-23 18:08:20 +0100

    chat now displays the messages

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err, db) {
 	if(err) throw err;
 
 	client.on('connection', function(socket) {
-		
 		var col = db.collection('messages');
 		var sendStatus = function(s) {
 			socket.emit('status', s);
 		};
+
+		// emit all messages
+		col.find().limit(100).sort({_id : 1}).toArray(function(err, res) {
+			if(err) throw err;
+			socket.emit('output', res);
+		});
 
 		// wait for input
 		socket.on('input', function(data) {
@@ -21,6 +26,9 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err, db) {
 				sendStatus('Name and message is required');
 			} else {
 				col.insert({name : name, message : message}, function() {
+					// emit latest message to ALL clients
+					client.emit('output', [data]);
+
 					sendStatus({
 						message : 'Message sent',
 						clear : true
